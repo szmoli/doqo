e21a19445676e50a88c000fb374675ca5102f25f
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2026-03-26 15:26:25 +0100

    comment cleanup

--- plugins/rust/src/processor.rs
+++ plugins/rust/src/processor.rs
@@ -6,7 +6,6 @@ use tree_sitter::Node;
 use crate::handlers::{handle_enum_item, handle_enum_variant, handle_function_item, handle_line_comment, handle_mod_item, handle_struct_item, handle_trait_item};
 
 pub struct RustProcessor {
-  //fn(node: Node, source: &'static str, context: ProcessingContext)
   handlers: HashMap<&'static str, NodeHandler>,
 }
 
@@ -51,58 +50,5 @@ impl LanguageProcessor for RustProcessor {
           false
         }
     }
-
-    /* 
-    fn create_symbol(
-        &self,
-        node: tree_sitter::Node,
-        source: &str,
-        context: &mut common::processor::ProcessingContext,
-    ) -> common::Symbol {
-        let symbol_source = &source[node.byte_range()];
-        let symbol_name = self.get_symbol_name(node, source);
-        let symbol_kind = node.kind().to_string();
-        let symbol_namespace = context.namespace_stack.clone();
-
-        // TODO: work out documentation logic.
-        /*
-        let symbol_documentation = if !context.comment_buffer.is_empty() {
-          let content = context.comment_buffer.clone();
-          context.comment_buffer.clear();
-          Some(Documentation::new(content))
-        } 
-        else {
-          None
-        };
-        */
-
-        Symbol::new(symbol_name, symbol_kind, symbol_namespace, symbol_source, None)
-    }
-    */
-
-    /*
-    fn get_symbol_name(&self, node: Node, source: &str) -> String {
-        let identifier_node = node.child_by_field_name("name");
-        let trait_node = node.child_by_field_name("trait");
-        let type_node = node.child_by_field_name("type");
-
-        //println!("{:?}, {:?}, {:?}", identifier_node, trait_node, type_node);
-
-        match (identifier_node, trait_node, type_node) {
-            (Some(identifier_node), None, None) => {
-              source[identifier_node.byte_range()].to_string()
-            }
-            (None, Some(trait_node), None) => {
-              source[trait_node.byte_range()].to_string()
-            }
-            (None, None, Some(type_node)) => {
-              source[type_node.byte_range()].to_string()
-            }
-            _ => {
-              format!("anonymous_{}", node.kind())
-            }
-        }
-    }
-    */
 }
 
